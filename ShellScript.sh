8a44258f8943a1e226afd6696f925f3b21f7a038
KarimMOthman <karimmohamedothman@gmail.com>
2018-10-27 22:49:00 +0200

    Befor DB connection and ssh to remote

--- ShellScript.sh
+++ ShellScript.sh
@@ -23,65 +23,78 @@
 #Implementation
 
 
+#init State
+#Define Repos Location (Path)
+Path[1]='/home/karimothman/CapacityRequest/Node1';
+Path[2]='/home/karimothman/CapacityRequest/Node2';
+Path[3]='/home/karimothman/CapacityRequest/Node3';
+Path[4]='/home/karimothman/CapacityRequest/Node4';
+
 #Part 1
 #Check If persistant variable/file exist (Contains latest CDRs that haven't processed yet)
 file="./LatestCDRs.txt"
-CDRfilePathOnSlaveNodes="/home/karimothman/CapacityRequest/Node1"
 if [ ! -f "$file" ]
 then
 
     printf "FirstNode\nSecondNode\nThirdNode\nFourthNode" > LatestCDRs.txt #if file not exist create a new empty one
 
-    FirstCDRName=''
-    SecondCDRName=''
-    ThirdCDRName=''
-    FourthCDRName=''
-    
+    CDRName[1]=''
+    CDRName[2]=''
+    CDRName[3]=''
+    CDRName[4]=''
+
     echo "$0: File '${file}' not found."
 
 else  # if file does exist store latest CDRs names in vaariables                                       #Refactoring Comment "those variables should be assigned in array" :)
-    FirstCDRName=`awk 'BEGIN{FS="|";OFS=" "}NR==1{print $1}' $file`
-    SecondCDRName=`awk 'BEGIN{FS="|";OFS=" "}NR==2{print $1}' $file`
-    ThirdCDRName=`awk 'BEGIN{FS="|";OFS=" "}NR==3{print $1}' $file`
-    FourthCDRName=`awk 'BEGIN{FS="|";OFS=" "}NR==4{print $1}' $file`
+    CDRName[1]=`awk 'BEGIN{FS="|";OFS=" "}NR==1{print $1}' $file`
+    CDRName[2]=`awk 'BEGIN{FS="|";OFS=" "}NR==2{print $1}' $file`
+    CDRName[3]=`awk 'BEGIN{FS="|";OFS=" "}NR==3{print $1}' $file`
+    CDRName[4]=`awk 'BEGIN{FS="|";OFS=" "}NR==4{print $1}' $file`
 fi
+
 
 
 
 
 #Part 2
-# If CDR/file still the latest we shouldn't process it as it may not been completed yet --> Data is incompleted
-# else
-    #We should operate on n-1 CDR/file and update our local persistant variable/file with latest CDR name
 
-LatestMOdifiedFileName=`cd $CDRfilePathOnSlaveNodes && find -name "*.cdr" -type f -printf "%Ts %p\n" | sort -n | tail -1 | sed -r -e 's/^[0-9]+ .//'`
-if [ "$LatestMOdifiedFileName" = "$FirstCDRName" ] #If CDR still the latest
-then
-    #CDR still the latest --> Do no thing
-    echo "Nothing1"
-else
-    if [ ! -f "$CDRfilePathOnSlaveNodes$FirstCDRName" ] #Note that at first run our persistant local file won't have any CDR to operate on --> Lw el local file fady .. 2aw el CDR msh mawgooda  3al slave machine 5alas
+#Loop on Nodes/ Path array
+for index in "${!Path[@]}"
+do
+    echo "$index"
+    # If CDR/file still the latest we shouldn't process it as it may not been completed yet --> Data is incompleted
+    # else
+        #We should operate on n-1 CDR/file and update our local persistant variable/file with latest CDR name
+    CDRfilePathOnSlaveNodes=${Path[$index]}
+    LatestMOdifiedFileName=`cd $CDRfilePathOnSlaveNodes && find -name "*.cdr" -type f -printf "%Ts %p\n" | sort -n | tail -1 | sed -r -e 's/^[0-9]+ .//'`
+    if [ "$LatestMOdifiedFileName" = "${CDRName[$index]}" ] #If CDR still the latest
     then
-    
-        #Access the first line of Local persistant file and overwrite it with the LatestMOdifiedFileName then write error in logs
-        sed -i "1s/.*/${LatestMOdifiedFileName////\\/}/" $file  # Will replace first line in file (1s/.*/) with latest CDR name ... ////\\/ this pattern to escape directory salsh /
+        #CDR still the latest --> Do no thing
+        echo ""
     else
-    
-        #Operate --> Parse
+        if [ ! -f "$CDRfilePathOnSlaveNodes${CDRName[$index]}" ] #Note that at first run our persistant local file won't have any CDR to operate on --> Lw el local file fady .. 2aw el CDR msh mawgooda  3al slave machine 5alas
+        then
+        
+            #Access the first line of Local persistant file and overwrite it with the LatestMOdifiedFileName then write error in logs
+            sed -i "${index}s/.*/${LatestMOdifiedFileName////\\/}/" $file  # Will replace first line in file (1s/.*/) with latest CDR name ... ////\\/ this pattern to escape directory salsh /
+        else
+        
+            #Operate --> Parse
 
-            while IFS= read -r var
-            do
+                while IFS= read -r var
+                do
 
-            FirstField=`echo "$var" | grep -oP '^([^,]+)'`  #First word before comma ^([^,]+)
-            SecondField=`echo "$var" | grep -oP '([^,]+)$'` #Last word after comman [^,]+)$
+                FirstField=`echo "$var" | grep -oP '^([^,]+)'`  #First word before comma ^([^,]+)
+                SecondField=`echo "$var" | grep -oP '([^,]+)$'` #Last word after comman [^,]+)$
 
-            #Insert in DB
-            echo "FirstField=$FirstField SecondField=$SecondField"  >> LocalDB.txt
-            
-            done < "$CDRfilePathOnSlaveNodes$FirstCDRName"
+                #Insert in DB
+                echo "FirstField=$FirstField SecondField=$SecondField"  >> LocalDB.txt
+                
+                done < "$CDRfilePathOnSlaveNodes${CDRName[$index]}"
 
-            
-        #Update our local persistant variable/file 
-        sed -i "1s/.*/${LatestMOdifiedFileName////\\/}/" $file  # Will replace first line in file (1s/.*/) with latest CDR name ... ////\\/ this pattern to escape directory salsh /
+                
+            #Update our local persistant variable/file 
+            sed -i "${index}s/.*/${LatestMOdifiedFileName////\\/}/" $file  # Will replace first line in file (1s/.*/) with latest CDR name ... ////\\/ this pattern to escape directory salsh /
+        fi
     fi
-fi
+done
